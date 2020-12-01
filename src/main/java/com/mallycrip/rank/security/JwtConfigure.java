667e5ff09f44918ad08ea8d1a2c10e6bba2ef833
Maly <migsking@naver.com>
2020-12-02 03:28:33 +0900

    [CREATE] JwtConfigure

---
+++ src/main/java/com/mallycrip/rank/security/JwtConfigure.java
@@ -0,0 +1,18 @@
+package com.mallycrip.rank.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtConfigure extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        JwtFilter jwtFilter = new JwtFilter(jwtProvider);
+        builder.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
