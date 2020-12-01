10154d848b2119cf183e92f8302846b236dd6f07
Maly <migsking@naver.com>
2020-12-02 03:31:57 +0900

    [CREATE] Web sec config

---
+++ src/main/java/com/mallycrip/rank/security/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.mallycrip.rank.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().disable()
+                .formLogin().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/account").permitAll()
+                .antMatchers(HttpMethod.POST,"/account/email").permitAll()
+                .and()
+                .apply(new JwtConfigure(jwtProvider));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
