c0f776bc879e8eefe9e3b2978f44ff9ff15c344e
Maly <migsking@naver.com>
2020-12-02 01:07:50 +0900

    [INIT]

---
+++ src/main/java/com/mallycrip/rank/RankApplication.java
@@ -0,0 +1,13 @@
+package com.mallycrip.rank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankApplication.class, args);
+	}
+
+}
